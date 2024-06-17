2cbf0e70776d3e2dbe1390e26221fb53fac688b4
Jonathan Reyes <jonathanr@tlgcohort.com>
2024-06-17 11:20:17 -0500

    added welcomescreen.txt, welcomeScreen() in SpAustintaneousApp

--- src/com/austinactivities/app/SpAustintaneousApp.java
+++ src/com/austinactivities/app/SpAustintaneousApp.java
@@ -1,5 +1,34 @@
 package com.austinactivities.app;
 
-class SpAustintaneousApp {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+import static com.apps.util.Console.*;
+
+public class SpAustintaneousApp {
+
+    public void execute() {
+        welcomeScreen();
+        //promptForNewOrOldUser();
+
+        //String user = promptForNewOrReturningUser();      //prompt screen asking if new or returning user
+        //userLoginScreen();
+        //showChoices();                                     //Choice Screen 1)Returning - dump recs/view recs  2)NEW user - shows choices of categories and asks to select from interests
+        //showBoard();
+        //updateBoard();      //will update
+    }
+
+
+    private void welcomeScreen(){
+        try {
+            clear();
+            String welcome = Files.readString(Path.of("resources/welcomescreen.txt"));
+            System.out.println("\n" + welcome + "\n");
+            pause(3000);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
