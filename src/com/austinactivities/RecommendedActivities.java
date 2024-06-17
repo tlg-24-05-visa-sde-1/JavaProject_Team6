ce67e298140187b85780e1caecadab54b78943d9
Jonathan Reyes <jonathanr@tlgcohort.com>
2024-06-17 16:58:21 -0500

    Added RecommendedActivities.java

---
+++ src/com/austinactivities/RecommendedActivities.java
@@ -0,0 +1,15 @@
+package com.austinactivities;
+
+
+import java.util.ArrayList;
+
+class RecommendedActivities{
+    ArrayList<Activity> generatedActivities = new ArrayList<>();
+
+    public ArrayList<Activity> generateRecommendedActivityList(Interests interests){
+        //TODO Returns a randomly generated list
+
+        return generatedActivities;
+    }
+
+}
