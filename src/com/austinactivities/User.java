44dd165c10281009e5f3513aae4d4409c43c3a8c
Jonathan Reyes <jonathanr@tlgcohort.com>
2024-06-17 20:25:04 -0500

    added Interest interaction with user class. Added getInterestList()

--- src/com/austinactivities/User.java
+++ src/com/austinactivities/User.java
@@ -1,14 +1,18 @@
 package com.austinactivities;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class User {
+public class User implements Serializable {
     private String userName;
-    private ArrayList<Interests> interestList=new ArrayList<>();
+    private List<Interests> interestList;
 
     public User() {
     }
     public User(String name) {
         setUser(userName);
+        this.interestList = new ArrayList<>();
     }
 
     public void setUser(String userName) {
@@ -20,7 +24,11 @@ public class User {
         return userName;
     }
 
+    public List<Interests> getInterestList() {
+        return Collections.unmodifiableList(interestList);
+    }
+
     public void addInterest(Interests i) {
-        addInterest(i);
+        interestList.add(i);
     }
 }
