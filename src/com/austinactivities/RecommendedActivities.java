01fc8b7c41311f4755ae7306623d4731952aca5d
Jonathan Reyes <jonathanr@tlgcohort.com>
2024-06-18 17:55:37 -0500

    Fixed an issue with RecommendedActivities.java

--- src/com/austinactivities/RecommendedActivities.java
+++ src/com/austinactivities/RecommendedActivities.java
@@ -5,51 +5,30 @@ import java.util.*;
 
 public class RecommendedActivities{
 
-    public static List<Activity> generateActivityList(List<Interests> listOfInterests){
+   public  List<Activity> generateActivityList(List<Interests> listOfInterests){
         List<Activity> generatedActivities = new ArrayList<>();
-        int wantedActRecs = 5;
+        //int wantedActRecs = 1;
 
         for (Interests interests : listOfInterests){
             generatedActivities.addAll(loadActivities(interests));
         }
-
         //Randomizes the list
         Collections.shuffle(generatedActivities, new Random());
 
         //Cuts down the list to yield number of recommended activities wanted
-        List<Activity>  cutDownListGeneratedActivities= generatedActivities.subList(0,wantedActRecs);
-        return cutDownListGeneratedActivities;
+        //List<Activity>  cutDownListGeneratedActivities= generatedActivities.subList(0,1);
+        //return cutDownListGeneratedActivities;
+       return generatedActivities;
     }
 
-    private static List<Activity> loadActivities(Interests interests){
+    private  List<Activity> loadActivities(Interests interests){
         List<Activity> activitiesOfInterest = new ArrayList<>();
-        //TODO
+
+        LoadActivitiesAndCreate start = new LoadActivitiesAndCreate();
+        start.loadActivitiesFromTxt("./resources/activities-examples.txt");
+        activitiesOfInterest.addAll(start.getActivitiesOnInterests(interests));
+
         return activitiesOfInterest;
     }
-
-    //WorkingExampleBelow
-
-    /*public static List<Activity> generateActivityList(List<Interests> listOfInterests){
-        List<Activity> generatedActivities = new ArrayList<>();
-
-        List<String> interestActivities = new ArrayList<>();
-        for (Interests interests : listOfInterests){
-            switch(interests){
-                case Water_Activity ->interestActivities.add("Water_Activity");
-                case Museums->interestActivities.add("Museums");
-                case Live_Music -> interestActivities.add("Live_Music");
-                case Food_Trucks -> interestActivities.add("Food_Trucks");
-                case Restaurants -> interestActivities.add("Restaurants");
-                default -> System.out.println("Unknown Interest: "+ interests);
-            }
-        }
-        System.out.println(interestActivities.toString());
-        Collections.shuffle(interestActivities, new Random());
-        System.out.println(interestActivities.toString());
-        List<String>  substringofList = interestActivities.subList(0, 2);
-        System.out.println(substringofList.toString());
-        return generatedActivities;
-    }*/
-
 
 }
