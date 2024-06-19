208a47521081e715d1faa87c6e034c7dab3eaf49
Jonathan Reyes <jonathanr@tlgcohort.com>
2024-06-19 13:54:45 -0500

    Committing the tests used during making of project

---
+++ test/com/austinactivities/RecommendedActivitiesTest.java
@@ -0,0 +1,50 @@
+package com.austinactivities;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import com.austinactivities.Interests.*;
+
+import static org.junit.Assert.*;
+
+public class RecommendedActivitiesTest {
+   /* List<Interests> sampleList;
+
+    @Before
+    public void setUp() {
+        sampleList = new ArrayList<>(Arrays.asList(Interests.Water_Activity, Interests.Museums, Interests.Live_Music,
+                Interests.Food_Trucks, Interests.Restaurants));
+    }
+
+    @Test
+    public void testGenerateActivityListSwitchStatement() {
+        List<Activity> sample = RecommendedActivities.generateActivityList(sampleList);
+        System.out.println(sample.toString());
+
+        sampleList = new ArrayList<>(Arrays.asList(Interests.Restaurants, Interests.Food_Trucks, Interests.Live_Music));
+        List<Activity> sample2 = RecommendedActivities.generateActivityList(sampleList);
+        System.out.println("Second set of samples: ");
+        System.out.println(sample2.toString());
+
+    }
+    @Test
+    public void testGenerateActivityListValidOutputs() {
+        List<Activity> sample = RecommendedActivities.generateActivityList(sampleList);
+        for (Activity a : sample) {
+            System.out.println(a);
+        }
+    }
+
+    @Test
+    public void testGenerateActivityListSwitchStatementReturnsCutDownList() {
+        List<Activity> sample = RecommendedActivities.generateActivityList(sampleList);
+        assertNotNull(sample);
+        assertEquals(3, sample.size());
+        for(Activity a : sample) {
+            System.out.println(a);
+        }
+    }*/
+}
---
+++ test/com/austinactivities/app/SpAustintaneousAppTest.java
@@ -0,0 +1,50 @@
+package com.austinactivities.app;
+
+import com.austinactivities.User;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SpAustintaneousAppTest {
+    SpAustintaneousApp sp = new SpAustintaneousApp();
+    User newUser = new User("jreyes");
+
+    //BELOW TESTS COMMENTED OUT DUE TO ERRORS WHEN MAKING THE METHODS PRIVATE
+
+/*
+    @Test
+    public void testPromptForNewOrReturningUser() {
+        sp.promptForNewOrReturningUser();
+    }
+
+    @Test
+    public void testMainMenu(){
+        sp.mainMenu();
+    }
+
+    @Test
+    public void testDisplayInterestMenu() {
+        sp.displayInterestsMenu();
+    }
+
+    @Test
+    public void testCreateAccount(){
+        sp.createAccount();
+    }
+
+    @Test
+    public void testGenerateRecommendationList() {
+        sp.generateRecommendationList();
+    }
+
+    @Test
+    public void testReturningToMainMenu() {
+        sp.returningToMainMenu();
+    }
+
+    @Test
+    public void testSelectInterestCategories(){
+        sp.selectInterestCategories();
+    }*/
+
+}
