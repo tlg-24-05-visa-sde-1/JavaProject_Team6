ebdf8c095de1491b2f0445b8cecd318f420bb92a
Jonathan Reyes <jonathanr@tlgcohort.com>
2024-06-19 13:44:19 -0500

    Users have their recommended activities now saved in their profile

--- src/com/austinactivities/User.java
+++ src/com/austinactivities/User.java
@@ -2,17 +2,20 @@ package com.austinactivities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class User implements Serializable {
     private String userName;
     private List<Interests> interestList;
+    private List<Activity> activityList;
 
     public User() {
     }
     public User(String name) {
         setUser(name);
         this.interestList = new ArrayList<>();
+        this.activityList = new ArrayList<>();
     }
 
     public void setUser(String userName) {
@@ -32,11 +35,20 @@ public class User implements Serializable {
         System.out.println(i + " has been added to your interests.");
     }
 
+    public void saveActivityList(ArrayList<Activity> activityList) {
+        this.activityList.addAll(activityList);
+    }
+
+    public List<Activity> getActivityList() {
+        return Collections.unmodifiableList(activityList);
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "userName='" + userName + '\'' +
                 ", interestList=" + interestList +
+                ", activityList=" + activityList +
                 '}';
     }
 }
